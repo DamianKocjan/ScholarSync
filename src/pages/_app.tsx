cbdb88b937e5b9b4ffc6cc56efc6c4662f7e1713
Damian Kocjan <DamianoKPL@wp.pl>
2023-10-21 15:52:24 +0200

    feat: finalize configuration of shadcn ui and add theme support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,41 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
+import { Inter as FontSans } from "next/font/google";
 
+import { ThemeProvider } from "~/components/theme-provider";
+import { cn } from "~/lib/utils";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+
+export const fontSans = FontSans({
+  subsets: ["latin-ext"],
+  variable: "--font-sans",
+});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <SessionProvider session={session}>
+        <div
+          className={cn(
+            "min-h-screen bg-background font-sans antialiased",
+            fontSans.variable,
+          )}
+        >
+          <Component {...pageProps} />
+        </div>
+      </SessionProvider>
+    </ThemeProvider>
   );
 };
 
