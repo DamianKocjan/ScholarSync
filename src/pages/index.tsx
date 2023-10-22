9a9cc1e2a1d525b68da03e5024be53d78249cb45
Jan Pałka <90349664+GeneralKartoniarz@users.noreply.github.com>
2023-10-22 14:32:07 +0200

    EventPost

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -41,9 +41,25 @@ interface ActivityPostProps {
 interface CreatePostProps{
   user: {name: string},
 }
+interface EventPostProps{
+  id: string,
+  title: string,
+  description: string,
+  from: Date,
+  to: Date,
+  location: string,
+  createdAt: Date,
+  updatedAt: Date,
+  user: {name: string},
+  userId: string,
+  interestedInEvent: any[],
+  comments: any[],
+  numberOfComments: number,
+  interactions: any[],
+}
 const ActivityPost : React.FC <ActivityPostProps> = ({title, createdAt, content, user, numberOfComments}) =>{
   return (
-    <Card className="w-fit min-w-[40rem] max-w-sm h-fit bg-slate-100 p-2 flex flex-col">
+    <Card className="w-fit min-w-[40rem] max-w-sm h-fit bg-slate-100 p-2 mb-5 flex flex-col">
       <CardHeader>
         <div className="flex flex-column gap-2 items-center">
           <Avatar>
@@ -68,7 +84,7 @@ const ActivityPost : React.FC <ActivityPostProps> = ({title, createdAt, content,
 } 
 const CreatePost : React.FC <CreatePostProps> = ({user}) =>{
   return (
-    <Card className="min-w-[30rem] w-fit max-w-xl h-fit bg-slate-100 p-2 flex flex-col">
+    <Card className="min-w-[30rem] w-fit max-w-xl h-fit bg-slate-100 p-2 mb-5 flex flex-col">
     <CardHeader>    
       <div className="flex flex-column justify-between gap-2 items-center">
         <div className="flex flex-column items-center">
@@ -99,6 +115,33 @@ const CreatePost : React.FC <CreatePostProps> = ({user}) =>{
     </Card>
   )
 }
+const EventPost : React.FC<EventPostProps> = ({user, createdAt, title, from, to, location, description, numberOfComments}) =>{
+  return(
+    <Card className="w-fit min-w-[40rem] max-w-sm h-fit bg-slate-100 p-2 mb-5 flex flex-col">
+      <CardHeader>
+        <div className="flex flex-column gap-2 items-center">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn"/>
+          </Avatar>
+          <div className="flex flex-col">
+            <p className="text-s">{user.name}</p>
+            <p className="text-xs font-thin">{createdAt.getDate()}</p>
+          </div>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <CardTitle>{title}</CardTitle>
+        <p className="text-xs">{from.getDate()} - {to.getDate()} {location}</p>
+        <p className="pt-3 pb-[1rem]">{description}</p>
+        <Button variant="default" className="w-full">I'm interested in this event!</Button>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+          <Button variant='outline'>Like</Button>
+          <Button variant='ghost'>Comments {numberOfComments}</Button>
+      </CardFooter>
+      </Card>
+  )
+}
 export default function Home() {
     const post1 = {
       type: "POST",
@@ -116,10 +159,28 @@ export default function Home() {
     const post2 ={
       user: {name: "Wach00nia"}
     } satisfies CreatePostProps
+    const post3 ={
+      id: "0",
+      title: "OMG IS IT A FNAF MOVIE",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum aliquid eum, amet reprehenderit dolor officiis sequi cumque quia aut maiores repudiandae perferendis quam odit, possimus nesciunt neque assumenda facilis commodi!",
+      from: new Date(), 
+      to: new Date(),
+      location: "Mojo Dojo Casa House",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      user: {name: 'Kartoniarz'},
+      userId: '0',
+      interestedInEvent: [],
+      comments: [],
+      numberOfComments: 2,
+      interactions: [],
+    } satisfies EventPostProps
   return (
       <>
-      <div className="flex justify-center p-10">
-      <CreatePost {...post2}/>
+      <div className="flex flex-col items-center justify-center p-10">
+          <EventPost {...post3}/>
+          <EventPost {...post3}/>
+          <EventPost {...post3}/>
       </div>
       </>
   );
