d2740ac25e74ab3651cdcfb3a8cdba8eca477295
Damian Kocjan <DamianoKPL@wp.pl>
2023-10-29 14:25:50 +0100

    fix: make for each note section types individual schema
    There was problem with zod schema validation when there was array of union of different objects. Union would merge all objects fields into one giant object. Which would make imposible to validate those sections on their type.

--- src/schemas/note.ts
+++ src/schemas/note.ts
@@ -15,7 +15,7 @@ export const noteWithIdSchema = noteSchema.extend({
   id: z.string(),
 });
 
-export const noteSection = z.object({
+export const basicNoteSection = z.object({
   type: z.literal("TEXT", {
     invalid_type_error: "Invalid section type.",
   }),
@@ -32,20 +32,104 @@ export const noteSection = z.object({
     .max(5000, "Description must not be longer than 5000 characters.")
     .optional(),
 });
-export type NoteSection = z.infer<typeof noteSection>;
+export type BasicNoteSection = z.infer<typeof basicNoteSection>;
 
-export const fileishNoteSection = noteSection.extend({
-  type: z.enum(["IMAGE", "VIDEO", "AUDIO", "FILE"], {
+export const imageNoteSection = z.object({
+  type: z.literal("IMAGE", {
     invalid_type_error: "Invalid section type.",
   }),
+  index: z
+    .number()
+    .min(0, "Index cannot be smaller than 0.")
+    .max(24, "Index cannot be larger than 24."),
+  subtitle: z
+    .string()
+    .max(250, "Subtitle must not be longer than 250 characters.")
+    .optional(),
+  content: z
+    .string()
+    .max(5000, "Description must not be longer than 5000 characters.")
+    .optional(),
   file: z.string().url("Invalid file URL."),
 });
-export type FileishNoteSection = z.infer<typeof fileishNoteSection>;
+export type ImageNoteSection = z.infer<typeof imageNoteSection>;
 
-export const quizNoteSection = noteSection.extend({
+export const videoNoteSection = z.object({
+  type: z.literal("VIDEO", {
+    invalid_type_error: "Invalid section type.",
+  }),
+  index: z
+    .number()
+    .min(0, "Index cannot be smaller than 0.")
+    .max(24, "Index cannot be larger than 24."),
+  subtitle: z
+    .string()
+    .max(250, "Subtitle must not be longer than 250 characters.")
+    .optional(),
+  content: z
+    .string()
+    .max(5000, "Description must not be longer than 5000 characters.")
+    .optional(),
+  file: z.string().url("Invalid file URL."),
+});
+export type VideoNoteSection = z.infer<typeof videoNoteSection>;
+
+export const audioNoteSection = z.object({
+  type: z.literal("AUDIO", {
+    invalid_type_error: "Invalid section type.",
+  }),
+  index: z
+    .number()
+    .min(0, "Index cannot be smaller than 0.")
+    .max(24, "Index cannot be larger than 24."),
+  subtitle: z
+    .string()
+    .max(250, "Subtitle must not be longer than 250 characters.")
+    .optional(),
+  content: z
+    .string()
+    .max(5000, "Description must not be longer than 5000 characters.")
+    .optional(),
+  file: z.string().url("Invalid file URL."),
+});
+export type AudioNoteSection = z.infer<typeof audioNoteSection>;
+
+export const fileNoteSection = z.object({
+  type: z.literal("FILE", {
+    invalid_type_error: "Invalid section type.",
+  }),
+  index: z
+    .number()
+    .min(0, "Index cannot be smaller than 0.")
+    .max(24, "Index cannot be larger than 24."),
+  subtitle: z
+    .string()
+    .max(250, "Subtitle must not be longer than 250 characters.")
+    .optional(),
+  content: z
+    .string()
+    .max(5000, "Description must not be longer than 5000 characters.")
+    .optional(),
+  file: z.string().url("Invalid file URL."),
+});
+export type FileNoteSection = z.infer<typeof fileNoteSection>;
+
+export const quizNoteSection = z.object({
   type: z.literal("QUIZ", {
     invalid_type_error: "Invalid section type.",
   }),
+  index: z
+    .number()
+    .min(0, "Index cannot be smaller than 0.")
+    .max(24, "Index cannot be larger than 24."),
+  subtitle: z
+    .string()
+    .max(250, "Subtitle must not be longer than 250 characters.")
+    .optional(),
+  content: z
+    .string()
+    .max(5000, "Description must not be longer than 5000 characters.")
+    .optional(),
   quizAnswers: z
     .array(
       z.object({
@@ -63,7 +147,16 @@ export type QuizNoteSection = z.infer<typeof quizNoteSection>;
 
 export const createNoteSchema = noteSchema.extend({
   sections: z
-    .array(z.union([noteSection, fileishNoteSection, quizNoteSection]))
+    .array(
+      z.union([
+        basicNoteSection,
+        imageNoteSection,
+        videoNoteSection,
+        audioNoteSection,
+        fileNoteSection,
+        quizNoteSection,
+      ]),
+    )
     .min(1, "Must have at least 1 section.")
     .max(25, "Must have at most 25 sections."),
 });
