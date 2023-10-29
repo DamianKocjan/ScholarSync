f5016789f72a0b6a4b1f8bcb1a91007fbb07ca7d
Damian Kocjan <DamianoKPL@wp.pl>
2023-10-29 14:03:50 +0100

    feat: create note page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { Inter as FontSans } from "next/font/google";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 import { ThemeProvider } from "~/components/theme-provider";
 import { cn } from "~/lib/utils";
@@ -26,14 +28,16 @@ const MyApp: AppType<{ session: Session | null }> = ({
       disableTransitionOnChange
     >
       <SessionProvider session={session}>
-        <div
-          className={cn(
-            "min-h-screen bg-background font-sans antialiased",
-            fontSans.variable,
-          )}
-        >
-          <Component {...pageProps} />
-        </div>
+        <DndProvider backend={HTML5Backend}>
+          <div
+            className={cn(
+              "min-h-screen bg-background font-sans antialiased",
+              fontSans.variable,
+            )}
+          >
+            <Component {...pageProps} />
+          </div>
+        </DndProvider>
       </SessionProvider>
     </ThemeProvider>
   );
---
+++ src/pages/notes/create.tsx
@@ -0,0 +1,265 @@
+import { LucideGripVertical, LucideTrash2 } from "lucide-react";
+import Head from "next/head";
+import { useDrag, useDrop } from "react-dnd";
+import { useFieldArray, useFormContext } from "react-hook-form";
+import { Button } from "~/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+import { Textarea } from "~/components/ui/textarea";
+import { useForm } from "~/hooks/use-form";
+import { createNoteSchema } from "~/schemas/note";
+import { api } from "~/utils/api";
+
+const ItemTypes = {
+  SECTION: "SECTION",
+};
+
+export default function NoteCreate() {
+  const form = useForm({
+    schema: createNoteSchema,
+    defaultValues: {
+      title: "",
+      description: "",
+      sections: [
+        {
+          type: "TEXT",
+          subtitle: "",
+          content: "",
+        },
+      ],
+    },
+  });
+  const sections = useFieldArray({
+    control: form.control,
+    name: "sections",
+  });
+
+  const appendSectionToNote = () => {
+    sections.append({
+      type: "TEXT",
+      subtitle: "",
+      content: "",
+      index: sections.fields.length,
+    });
+  };
+
+  const moveSection = (fromIndex: number, toIndex: number) => {
+    sections.move(fromIndex, toIndex);
+  };
+
+  const removeSection = (index: number) => {
+    if (sections.fields.length === 1) {
+      return;
+    }
+
+    sections.remove(index);
+  };
+
+  const {} = api.note.create.useMutation();
+
+  return (
+    <>
+      <Head>
+        <title>Create note</title>
+      </Head>
+      <main className="flex flex-col items-center">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(console.log)}
+            className="w-2/3 space-y-6"
+          >
+            {sections.fields.map((field, index) => {
+              if (index === 0) {
+                return (
+                  <div className="flex space-x-6" key={field.id}>
+                    <NoteDetails />
+                    <div className="relative w-full">
+                      <NoteSection
+                        index={index}
+                        isLast={index === sections.fields.length - 1}
+                        handleAppendSection={appendSectionToNote}
+                        handleMoveSection={moveSection}
+                        handleRemoveSection={removeSection}
+                      />
+                    </div>
+                  </div>
+                );
+              }
+              return (
+                <NoteSection
+                  key={field.id}
+                  index={index}
+                  isLast={index === sections.fields.length - 1}
+                  handleAppendSection={appendSectionToNote}
+                  handleMoveSection={moveSection}
+                  handleRemoveSection={removeSection}
+                />
+              );
+            })}
+
+            <Button type="submit" className="w-full">
+              Create note
+            </Button>
+          </form>
+        </Form>
+      </main>
+    </>
+  );
+}
+
+function NoteDetails() {
+  const { control } = useFormContext();
+
+  return (
+    <div className="w-1/3 space-y-6 border-r pr-6">
+      <FormField
+        control={control}
+        name="title"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Title</FormLabel>
+            <FormControl>
+              <Input placeholder="Note title" {...field} />
+            </FormControl>
+            <FormDescription>This is your note title.</FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={control}
+        name="description"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Title</FormLabel>
+            <FormControl>
+              <Textarea placeholder="Note description" {...field} />
+            </FormControl>
+            <FormDescription>
+              This is your note description. It should be short and concise.
+            </FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    </div>
+  );
+}
+
+type NoteSectionProps = {
+  index: number;
+  isLast: boolean;
+  handleAppendSection: () => void;
+  handleMoveSection: (from: number, to: number) => void;
+  handleRemoveSection: (index: number) => void;
+};
+
+function NoteSection({
+  index,
+  isLast,
+  handleAppendSection,
+  handleMoveSection,
+  handleRemoveSection,
+}: NoteSectionProps) {
+  const { control } = useFormContext();
+
+  const [{ isDragging }, ref, preview] = useDrag({
+    type: ItemTypes.SECTION,
+    item: { index },
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  });
+
+  const [, drop] = useDrop({
+    accept: ItemTypes.SECTION,
+    hover: (draggedItem: { index: number }) => {
+      if (draggedItem.index !== index) {
+        handleMoveSection(draggedItem.index, index);
+        draggedItem.index = index;
+      }
+    },
+  });
+
+  return (
+    <>
+      <div
+        className="relative space-y-6"
+        ref={(node) => preview(drop(node))}
+        style={{
+          opacity: isDragging ? 0.5 : 1,
+        }}
+      >
+        <div className="absolute right-0 top-0 space-x-2">
+          <Button
+            type="button"
+            variant="ghost"
+            size="icon"
+            onClick={() => handleRemoveSection(index)}
+          >
+            <LucideTrash2 className="h-4 w-4" />
+          </Button>
+
+          <Button
+            type="button"
+            variant="ghost"
+            size="icon"
+            className="cursor-move"
+            ref={ref}
+          >
+            <LucideGripVertical className="h-4 w-4" />
+          </Button>
+        </div>
+
+        <FormField
+          control={control}
+          name={`sections.${index}.subtitle`}
+          render={({ field }) => (
+            <FormItem className="pt-8">
+              <FormLabel>Subtitle</FormLabel>
+              <FormControl>
+                <Input placeholder="Note subtitle" {...field} />
+              </FormControl>
+              <FormDescription>
+                Section subtitle. It should be short and explanatory of the
+                content of the section.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={control}
+          name={`sections.${index}.content`}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Content</FormLabel>
+              <FormControl>
+                <Textarea placeholder="Note content" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </div>
+
+      {isLast ? (
+        <Button
+          type="button"
+          className="my-6 w-full"
+          onClick={handleAppendSection}
+        >
+          <span>Add section</span>
+        </Button>
+      ) : null}
+    </>
+  );
+}
